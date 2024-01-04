82765efc8963094dca7caa42aa92164529de7833
Mitchell Homer <mitchell@accurx.com>
2024-01-04 16:04:44 +0000

    Adapted configuration for a single cronjob setup and simplified configuration, ready for multiple usescases. We should be able to use this in our current configuration once the repo is public. Or if I publish the chart for internal consumption

---
+++ chart/internal-certificate-heartbeat.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+expire_threshold_s=$1
+
+cert_array=("${@:1}")
+for arg in "${cert_array[@]}"; do
+  IFS=: read cert_name heartbeat_url <<< "$arg"
+  set -x;
+  kubectl get secret "$cert_name" --namespace default -o "jsonpath={.data['tls\.crt']}" | base64 -d | openssl x509 --checkend $expire_threshold_s -noout
+  set +x;
+  result=$?
+  if [ $result -ne 0 ]; then
+    printf "Certificate '%s' will expire within %s seconds" "$cert_name" "$expire_threshold_s";
+  else
+    set -x;
+    curl -X POST "$heartbeat_url" --fail
+    set +x;
+    printf "\nSent heartbeat for '%s'" "$cert_name"
+  fi
+done
+
