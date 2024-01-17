7743e4f83f962b5af5f3f209f2c7097b206205f2
Mitchell Homer <mitchell@accurx.com>
2024-01-17 17:32:00 +0000

    rework the script and cronjob crd structure to allow invoking the shell script multiple times, for each cert.
    This provides some benefits:
    - avoids the need for yaml/json config manipulation inside the script process.
    - makes the script easier to test/debug.
    - will mean the script can be invoked multiple times without erroring
      out once.
    - more complex configuration options/overrides can be provided for
      different alert thresholds and other options.

--- chart/internal-certificate-heartbeat.sh
+++ chart/internal-certificate-heartbeat.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 expire_threshold_s=$1
+secret_name=$2
+heartbeat_url=$3
 
-cert_array=("${@:2}")
-for arg in "${cert_array[@]}"; do
-  IFS=: read cert_name heartbeat_url <<< "$arg"
-  set -x;
-  kubectl get secret "$cert_name" --namespace default -o "jsonpath={.data['tls\.crt']}" | base64 -d | openssl x509 --checkend $expire_threshold_s -noout
-  result=$?
-  set +x;
-  if [ $result -ne 0 ]; then
-    printf "Certificate '%s' will expire within %s seconds" "$cert_name" "$expire_threshold_s";
-  else
+set -x;
+kubectl get secret "$secret_name" --namespace default -o "jsonpath={.data['tls\.crt']}" | base64 -d | openssl x509 --checkend $expire_threshold_s -noout
+set +x;
+
+result=$?
+if [ $result -ne 0 ]; then
+    printf "Certificate '%s' will expire within %s seconds" "$secret_name" "$expire_threshold_s";
+else
     set -x;
     curl -X POST "$heartbeat_url" --fail
     set +x;
-    printf "\nSent heartbeat for '%s'" "$cert_name"
-  fi
-done
+    printf "\nSent heartbeat for '%s'" "$secret_name"
+fi
 
